241d009af58a13c53de579b432bbeb6cf70a25fd
曹小帅 <yashcao@163.com>
2019-05-27 21:11:59 +0800

    Update Dirichlet_Sinc.m

--- Beam Space MIMO/Dirichlet_Sinc.m
+++ Beam Space MIMO/Dirichlet_Sinc.m
@@ -10,16 +10,16 @@ y1(201)=n(1);
 y2(201)=n(2);
 
 % subplot(2, 1, 1);
-plot(t, y1, 'o--b');
+plot(t, y1, '--b', 'linewidth', 1);
 hold on;
-plot(t, y2, 's-r');
+plot(t, y2, '-r', 'linewidth', 1);
 
 grid on;
-title('$\frac{\sin(\pi n t )}{\sin(\pi t)}$', 'Interpreter', 'Latex');
-xlabel('$t$','Interpreter','LaTex');
-ylabel('$y$','Interpreter','LaTex');
+title('$\frac{\sin(\pi n t )}{\sin(\pi t)}$', 'Interpreter', 'Latex', 'fontsize', 18);
+xlabel('$t$','Interpreter','LaTex', 'fontsize', 15);
+ylabel('$y$','Interpreter','LaTex', 'fontsize', 15);
 l = legend('$n=7$', '$n=9$');
-set(l,'Interpreter','latex');
+set(l,'Interpreter','latex', 'fontsize', 12);
 
 
 % y1 = sin(n*pi*(t-0.5))./sin(pi*(t-0.5));
